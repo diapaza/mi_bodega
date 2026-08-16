857396b2034be4b89fba3489615d6e148b70c715
VirtualAI3 <elacaguate123@gmail.com>
2026-08-16 16:23:32 -0500

    chore: actualizar plugin registrants tras agregar gal
    - Actualizar GeneratedPluginRegistrant para macOS y Windows
    - Actualizar generated_plugins.cmake para Windows
    - Reflejar nueva dependencia gal en registros de plugins

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,10 +7,12 @@ import Foundation
 
 import file_selector_macos
 import flutter_secure_storage_darwin
+import gal
 import google_sign_in_ios
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FlutterSecureStorageDarwinPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageDarwinPlugin"))
+  GalPlugin.register(with: registry.registrar(forPlugin: "GalPlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
 }
